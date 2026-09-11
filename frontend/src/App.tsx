6a1a9248680058ac6a86b4a73f0412e00497fee4
Vladimir Kravtsov <you@example.com>
2026-09-11 18:46:55 +0300

    Target State

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,12 @@
 import CalculatePage from './pages/CalculatePage.tsx'
+import useBackend from './hooks/BackendHook.tsx'
 
 function App() {
 
+    const [calculate, fetchHistory] = useBackend()
+
     return (
-        CalculatePage()
+        <CalculatePage onCalculate={calculate} onFetchHistory={fetchHistory} />
     )
 }
 
